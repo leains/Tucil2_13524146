a9268c8e8f6e95007a11a3fa39c9166d11f25125
leains <leonardusbrain0@gmail.com>
2026-03-28 20:53:30 +0700

    Menambahkan hasil build (compile) program

---
+++ bin/CMakeFiles/main.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "C:/Brain/ITB/SEM_4/STIGMA/TUCIL2/src/dnc.cpp" "CMakeFiles/main.dir/src/dnc.cpp.obj" "gcc" "CMakeFiles/main.dir/src/dnc.cpp.obj.d"
+  "C:/Brain/ITB/SEM_4/STIGMA/TUCIL2/src/io.cpp" "CMakeFiles/main.dir/src/io.cpp.obj" "gcc" "CMakeFiles/main.dir/src/io.cpp.obj.d"
+  "C:/Brain/ITB/SEM_4/STIGMA/TUCIL2/src/main.cpp" "CMakeFiles/main.dir/src/main.cpp.obj" "gcc" "CMakeFiles/main.dir/src/main.cpp.obj.d"
+  "C:/Brain/ITB/SEM_4/STIGMA/TUCIL2/src/octree.cpp" "CMakeFiles/main.dir/src/octree.cpp.obj" "gcc" "CMakeFiles/main.dir/src/octree.cpp.obj.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
